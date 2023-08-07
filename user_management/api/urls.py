8ed73f7f76be6ee4502a2758c1699b713359a3c2
Irina Poltarykhina <irina.poltarykhina@gmail.com>
2023-08-07 18:13:18 +0700

    finish frontend

--- user_management/api/urls.py
+++ user_management/api/urls.py
@@ -1,13 +1,9 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import UserViewSet
-
 app_name = 'api'
 
 router = DefaultRouter()
-
-router.register(r'users', UserViewSet, basename='users')
 
 
 urlpatterns = [
--- user_management/users/models.py
+++ user_management/users/models.py
@@ -6,8 +6,8 @@ from django.db import models
 class User(AbstractUser):
     email = models.EmailField(max_length=254, unique=True)
     username = models.CharField(max_length=150, unique=True)
-    first_name = models.CharField(max_length=150)
-    last_name = models.CharField(max_length=150)
+    first_name = models.CharField(max_length=150, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
     password = models.CharField(max_length=150)
     picture = models.ImageField(
         blank=True,
